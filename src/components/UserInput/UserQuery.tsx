be348a7a0f2d64b63a0efa8b5af2205dc06a28ae
Prasad Shinde <sketchware1@gmail.com>
2023-09-12 18:42:46 +0530

    changes

--- src/components/UserInput/UserQuery.tsx
+++ src/components/UserInput/UserQuery.tsx
@@ -1,7 +1,10 @@
 import { IonIcon } from "@ionic/react";
-import { sendOutline } from "ionicons/icons";
+import classNames from "classnames";
+import { sendOutline, send } from "ionicons/icons";
+import { useState } from "react";
 
 export default function UserQuery() {
+  const [query, setQuery] = useState("");
   return (
     <form className="input shadow-md dark:bg-[#40414f] p-2 border dark:border-none flex items-center   rounded-md">
       <div className="w-11/12">
@@ -9,11 +12,18 @@ export default function UserQuery() {
           name="query"
           className="h-6 px-2  w-full outline-none resize-none dark:bg-transparent dark:text-white"
           placeholder="Send a message"
+          onChange={(e) => setQuery(e.target.value)}
         ></textarea>
       </div>
       <div className=" w-1/12 text-center">
-        <button type="submit" className=" text-center text-gray-200 ">
-          <IonIcon icon={sendOutline} />
+        <button
+          type="submit"
+          className={classNames(
+            " text-center text-gray-200 transition inline-flex items-center justify-center py-2 px-2 rounded-md",
+            { "bg-green-500": query }
+          )}
+        >
+          <IonIcon icon={query ? send : sendOutline} />
         </button>
       </div>
     </form>
