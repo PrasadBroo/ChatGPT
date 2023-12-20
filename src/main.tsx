5e89a57843096be46d60ce10b739409622c27d80
Prasad Shinde <sketchware1@gmail.com>
2023-12-20 20:46:39 +0530

    added postcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,9 @@
-import React, { Profiler } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./assets/css/output.css";
+import "./assets/css/main.css";
 import "@ionic/react/css/core.css";
 
-function onRender(
-  id: string,
-  phase: "mount" | "update",
-  actualDuration: number,
-  baseDuration: number,
-  startTime: number,
-  commitTime: number,
-  interactions: Set<any>
-) {
-  console.log(
-    id,
-    phase,
-    actualDuration,
-    baseDuration,
-    startTime,
-    commitTime,
-    interactions
-  );
-}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
